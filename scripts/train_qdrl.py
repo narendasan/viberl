ec1dece82a3983f6af3a879eda19e468648979b6
Naren Dasan <naren@narendasan.com>
2025-03-19 16:39:46 -0600

    Make policies without reusing the full trainstate

--- scripts/train_qdrl.py
+++ scripts/train_qdrl.py
@@ -6,6 +6,8 @@ from brax import envs
 from brax.io import html
 from orbax.checkpoint import tree
 from rejax import PPO
+import flax
+import jax.numpy as jnp
 
 from rl_sandbox.utils import (
     argparser,
@@ -35,13 +37,32 @@ algo = PPO.create(**config["algorithm"])
 jit_train = jax.jit(algo.train)
 train_state, results = jit_train(root_key)
 
-flattened_train_state, restore_fn = jax.flatten_util.ravel_pytree(train_state)
-print(flattened_train_state.shape)
+# Extract and flatten Actor params
+params = train_state.actor_ts.params
+flattened_params, restore_fn = jax.flatten_util.ravel_pytree(params)
+print(flattened_params.shape)
 
-reconstructed_train_state = restore_fn(flattened_train_state)
+reconstructed_params = restore_fn(flattened_params)
 
-restored_algo = PPO.create(**config["algorithm"])
-jit_policy = jax.jit(restored_algo.make_act(reconstructed_train_state))
+
+def make_act(ppo: PPO, ts: flax.training.train_state.TrainState, actor_params: flax.core.FrozenDict):
+    @jax.jit
+    def act(obs, rng):
+        if getattr(ppo, "normalize_observations", False):
+            obs = ppo.normalize_obs(ts.obs_rms_state, obs)
+
+        obs = jnp.expand_dims(obs, 0)
+        action = ppo.actor.apply(actor_params, obs, rng, method="act")
+        return jnp.squeeze(action)
+
+    return act
+
+
+#reconstructed_train_state = train_state.replace(actor_ts=train_state.actor_ts.replace(params=reconstructed_params))
+#restored_algo = PPO.create(**config["algorithm"])
+#jit_policy = jax.jit(restored_algo.make_act(reconstructed_train_state))
+
+jit_policy = make_act(algo, train_state, reconstructed_params)
 
 vis, reward = render_gymnax(jit_policy, config, root_key)
 vis.animate(f"{os.getcwd()}/{config['experiment']['results_dir']}/restored_ppo.gif")
