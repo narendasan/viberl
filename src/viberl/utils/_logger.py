042356ba874d9f7c1ac5915a7f8835df7029b7ae
Naren Dasan <naren@narendasan.com>
2025-03-30 15:48:19 -0600

    Adding logging settings

--- src/viberl/utils/_logger.py
+++ src/viberl/utils/_logger.py
@@ -5,7 +5,8 @@ from typing import Any, Dict
 
 def setup_logger(config: Dict[str,Any]) -> None:
     os.makedirs(f"{os.getcwd()}/{config['experiment']['log_dir']}", exist_ok=True)
-    logging.basicConfig(level=logging.INFO,
+    lvl = logging.getLevelName(config['experiment']['log_level']) if 'log_level' in config['experiment'] else logging.INFO
+    logging.basicConfig(level=lvl,
         handlers = [
             logging.FileHandler(f"{config['experiment']['log_dir']}/{config['experiment']['experiment_name']}.log"),
             logging.StreamHandler()
