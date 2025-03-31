c0c7acbf8c44da14619b85894be17af9d7206174
Naren Dasan <naren@narendasan.com>
2025-03-30 23:04:46 -0600

    partially through minimizing ppo state

--- scripts/train_qdrl.py
+++ scripts/train_qdrl.py
@@ -4,8 +4,10 @@ import os
 import flax
 import jax
 import jax.numpy as jnp
-from rejax import PPO
+#from rejax import PPO
+from viberl.algorithms._vppo import VPPO
 
+import viberl
 from viberl.env import render_gymnax
 from viberl.utils import (
     argparser,
@@ -22,14 +24,17 @@ _LOGGER = logging.getLogger(__name__)
 
 # Use PRNGKey since theres some limitations with .key
 root_key = jax.random.PRNGKey(config["experiment"]["root_seed"])
+agent_keys = jax.random.split(root_key, config["experiment"]["num_agent_seeds"])
 
-algo = PPO.create(**config["algorithm"])
+algo = VPPO.create(**config["algorithm"])
 
-jit_train = jax.jit(algo.train)
-train_state, results = jit_train(root_key)
+jit_train = jax.jit(jax.vmap(algo.train))
+train_states, results = jit_train(agent_keys)
+
+agents = viberl.utils.tree_unstack(train_states)
 
 # Extract and flatten Actor params
-params = train_state.actor_ts.params
+params = agents[0].actor_ts.params
 flattened_params, restore_fn = jax.flatten_util.ravel_pytree(params)
 print(flattened_params.shape)
 
@@ -37,7 +42,7 @@ reconstructed_params = restore_fn(flattened_params)
 
 
 def make_act(
-    ppo: PPO,
+    ppo: VPPO,
     ts: flax.training.train_state.TrainState,
     actor_params: flax.core.FrozenDict,
 ):
@@ -57,7 +62,7 @@ def make_act(
 # restored_algo = PPO.create(**config["algorithm"])
 # jit_policy = jax.jit(restored_algo.make_act(reconstructed_train_state))
 
-jit_policy = make_act(algo, train_state, reconstructed_params)
+jit_policy = make_act(algo, agents[0], reconstructed_params)
 
 vis, reward = render_gymnax(jit_policy, config, root_key)
 vis.animate(f"{os.getcwd()}/{config['experiment']['results_dir']}/restored_ppo.gif")
