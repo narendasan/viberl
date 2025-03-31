715805abbbabdefe4fd0f130f23da1672ed35107
Naren Dasan <naren@narendasan.com>
2025-03-30 22:00:45 -0600

    suppressing new orbax logging messages

--- src/viberl/utils/_logger.py
+++ src/viberl/utils/_logger.py
@@ -2,6 +2,8 @@ import logging
 import os
 from typing import Any, Dict
 
+import absl.logging
+absl.logging.set_verbosity(absl.logging.WARNING)
 
 def setup_logger(config: Dict[str, Any]) -> None:
     os.makedirs(f"{os.getcwd()}/{config['experiment']['log_dir']}", exist_ok=True)
@@ -10,16 +12,42 @@ def setup_logger(config: Dict[str, Any]) -> None:
         if "log_level" in config["experiment"]
         else logging.INFO
     )
-    logging.basicConfig(
-        level=lvl,
-        handlers=[
-            logging.FileHandler(
-                f"{config['experiment']['log_dir']}/{config['experiment']['experiment_name']}.log"
-            ),
-            logging.StreamHandler(),
-        ],
-        force=True,  # Needed since orbax does some weird stuff
-    )
+    logging.config.dictConfig({
+            "version": 1,
+            "disable_existing_loggers": True if lvl != logging.DEBUG else False,
+            "formatters": {
+                "brief": {
+                    "format": "%(asctime)s - %(levelname)s - %(message)s",
+                    "datefmt": "%H:%M:%S",
+                },
+                "standard": {
+                    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                },
+            },
+            "handlers": {
+                "file": {
+                    "level": lvl,
+                    "class": "logging.FileHandler",
+                    "filename": f"{config['experiment']['log_dir']}/{config['experiment']['experiment_name']}.log",
+                    "formatter": "standard",
+                },
+                "console": {
+                    "level": lvl,
+                    "class": "logging.StreamHandler",
+                    "formatter": "brief",
+                },
+            },
+            "loggers": {
+                "": {  # root logger
+                    "handlers": ["file", "console"],
+                    "level": lvl,
+                    "propagate": True,
+                },
+            },
+            "force": True,  # Needed since orbax does some weird stuff
+        })
+
     logging.getLogger("jax._src.xla_bridge").propagate = False
     logging.getLogger("absl").propagate = False
     return
